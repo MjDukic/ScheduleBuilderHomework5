0b8d82a2505d3b625f5fc071b5f43b20af339c70
MJ <mirjanadukicc@gmail.com>
2022-10-31 22:46:33 -0700

    added 9-5 schedule in html with button

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,9 @@
-var container = document.querySelector("container");
-var blocks = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+// var container = document.querySelector("container");
+// var blocks = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-for (var i = 0; i < blocks.length; i++) {
-    var block = document.createElement("div");
-    
-    block.textContent = moment(blocks[i], 'HH').format('h a');
-    container.appendChild(block);
-}
+// for (var i = 0; i < blocks.length; i++) {
+//     var block = document.createElement("div");
+
+//     block.textContent = moment(blocks[i], 'HH').format('h a');
+//     container.appendChild(block);
+// }
