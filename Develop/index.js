68410924b7652730eb84904beee818f93decf187
MJ <mirjanadukicc@gmail.com>
2022-10-31 23:12:31 -0700

    added personal touch to css and starting js

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,4 @@
-// var container = document.querySelector("container");
-// var blocks = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+$(document).ready(function () {
+    
 
-// for (var i = 0; i < blocks.length; i++) {
-//     var block = document.createElement("div");
-
-//     block.textContent = moment(blocks[i], 'HH').format('h a');
-//     container.appendChild(block);
-// }
+});
